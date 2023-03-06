84e4fb155ba247358c52040386eb1d280f6ec27b
farhan <nahidahmed9933@gmail.com>
2023-03-07 00:28:11 +0600

    layout added

---
+++ src/routes/(auth)/password/+layout@.svelte
@@ -0,0 +1,3 @@
+<h1>Password layout header</h1>
+<slot />
+<h1>Password layout footer</h1>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,1 +1,15 @@
+<script lang="ts">
+	import { afterNavigate, beforeNavigate, goto } from '$app/navigation';
+
+	const handelClick = () => {
+		// goto('/products', { replaceState: true });
+		goto('/');
+	};
+
+	beforeNavigate((navigation) => console.log({ before: navigation }));
+	afterNavigate((navigation) => console.log({ after: navigation }));
+</script>
+
 <h1>Products</h1>
+
+<button on:click={handelClick}>Home</button>
