d1e710a5ad80afeee6e31a09bad3fdcd930f9820
farhan <nahidahmed9933@gmail.com>
2023-03-06 23:21:07 +0600

    nested route , dynamic route, catch all route &  optional parameter

---
+++ src/routes/docs/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Features root page</h1>
---
+++ src/routes/products/[productId]/reviews/[reviewId]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	type propsType = {
+		productId: string;
+		reviewId: string;
+	};
+
+	const { productId, reviewId } = $page.params;
+</script>
+
+<h1>
+	Review {reviewId} for product {productId}
+</h1>
